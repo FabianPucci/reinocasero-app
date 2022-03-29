56003fc16b13e1ee0212db22b05112481671116e
Fabian Pucci <you@example.com>
2022-03-29 20:02:02 -0300

    timePicker modificado

--- src/components/molecules/InputAndHour/inputAndHour.component.jsx
+++ src/components/molecules/InputAndHour/inputAndHour.component.jsx
@@ -1,26 +1,20 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import styles from './inputAndHour.style';
-import TextInput from '../../atoms/TextInput/textInput.component';
-import DatePicker from 'react-native-datepicker';
+import { View, Text, Button } from "react-native";
+import React from "react";
+import styles from "./inputAndHour.style";
+import TextInput from "../../atoms/TextInput/textInput.component";
+import { DateTimePickerAndroid } from "@react-native-community/datetimepicker";
 
 const InputAndHour = (props) => {
-
   return (
     <View style={styles.container}>
-        <TextInput placeholder="Cliente" value={props.value} onChangeText={props.onChangeText} />
-        <DatePicker 
-          mode='time'
-          style={styles.timePicker}
-          date={props.date}
-          confirmBtnText="Confirmar"
-          cancelBtnText="Cancelar"
-          showIcon={false}
-          onDateChange={props.onDateChange}
-          useNativeDriver={false}
-        />
+      <TextInput
+        placeholder="Cliente"
+        value={props.value}
+        onChangeText={props.onChangeText}
+      />
+      <Button onPress={props.onChangeDate} title="horario de entrega" />
     </View>
-  )
-}
+  );
+};
 
-export default InputAndHour
+export default InputAndHour;
--- src/components/screens/AddOrder/addOrderScreen.component.jsx
+++ src/components/screens/AddOrder/addOrderScreen.component.jsx
@@ -1,35 +1,58 @@
-import { View } from 'react-native'
-import React, {useState} from 'react'
-import InputAndHour from '../../molecules/InputAndHour/inputAndHour.component'
-import styles from './addOrderScreen.style';
-import { Button } from 'native-base';
-import AddMenuItem from '../../molecules/AddMenuItem/addMenuItem.component';
-import Subtitle from '../../atoms/Subtitle/subtitle.component';
+import { View } from "react-native";
+import React, { useState } from "react";
+import InputAndHour from "../../molecules/InputAndHour/inputAndHour.component";
+import styles from "./addOrderScreen.style";
+import { Button } from "native-base";
+import AddMenuItem from "../../molecules/AddMenuItem/addMenuItem.component";
+import Subtitle from "../../atoms/Subtitle/subtitle.component";
+import { DateTimePickerAndroid } from "@react-native-community/datetimepicker";
 
 const AddOrderScreen = () => {
   const [dateValue, setDateValue] = useState(new Date());
-  const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState("");
   const [menues, setMenues] = useState([]);
+  const [hourText, setHourText] = useState("empty");
 
-  const handleDate = (time) => {
-    console.log(time)
-    setDateValue(time);
+  const handleDate = () => {
+    DateTimePickerAndroid.open({
+      testID: "dateTimePicker",
+      value: dateValue,
+      mode: "time",
+      is24Hour: true,
+      display: "default",
+      onChange,
+    });
   };
 
-  const handleInput = text => {
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate;
+    setDateValue(currentDate);
+
+    const tempDate = new Date(currentDate);
+    const finalTime = tempDate.getHours() + ":" + tempDate.getMinutes();
+    setHourText(finalTime);
+  };
+
+  const handleInput = (text) => {
     setInputValue(text);
-  }
+  };
 
   return (
     <View style={styles.container}>
       <View style={styles.center}>
-        <InputAndHour date={dateValue} onDateChange={handleDate} value={inputValue} onChangeText={handleInput} />
-        <Subtitle text={`${menues.length} menúes agregados`} />
+        <InputAndHour
+          date={dateValue}
+          onChangeDate={handleDate}
+          value={inputValue}
+          onChangeText={handleInput}
+        />
+        <Subtitle
+          text={`${menues.length} menúes agregados para las ${hourText}`}
+        />
         <AddMenuItem />
       </View>
     </View>
-  )
-}
+  );
+};
 
-
-export default AddOrderScreen
+export default AddOrderScreen;
